ab7e81a6298917c42b55dabf356dac8470c79295
misterh100 <handsomenyathi1@gmail.com>
2024-02-04 15:54:03 +0200

    change metadata

--- app/[names]/layout.tsx
+++ app/[names]/layout.tsx
@@ -1,4 +1,9 @@
+import type { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "Its love month,just a little poem for someone special",
+  description: "Hope you like it",
+};
 
 export default function LoveLayout({
   children,
